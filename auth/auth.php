353c4198ab77a5249fbfe14de2a2f537f8e87024
Choomai <nguyencaonguyen0944322545@gmail.com>
2025-09-02 01:55:31 +0700

    Create the table (if not existed) on connection

--- auth/auth.php
+++ auth/auth.php
@@ -14,7 +14,34 @@ $usr_pattern = '/^(?!.*([_.])\\1)[a-zA-Z0-9._]{3,32}$/';
 class Auth {
     public $db;
     public $sql_cmd;
-    function __construct($inp_db) {$this->db = $inp_db;}
+    function __construct($inp_db) {
+        $this->db = $inp_db;
+        $this->db->query("CREATE TABLE IF NOT EXISTS `account_list`(
+            `id` int NOT NULL AUTO_INCREMENT,
+            `username` varchar(32) NOT NULL,
+            `password` char(60) NOT NULL,
+            `target_host` varchar(255) NOT NULL DEFAULT 'localhost',
+            `target_port` int NOT NULL DEFAULT '25575',
+            `target_password` varchar(255) NOT NULL DEFAULT 'password',
+            `priority` int DEFAULT '0',
+            PRIMARY KEY (`id`),
+            UNIQUE KEY `sqlite_autoindex_account_list_1` (`username`)
+        )");
+        $this->db->query("CREATE TABLE IF NOT EXISTS `auth_logs` (
+            `id` int NOT NULL AUTO_INCREMENT,
+            `time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+            `info` text,
+            PRIMARY KEY (`id`)
+        )");
+
+        $this->db->query("CREATE TABLE IF NOT EXISTS `user_logs` (
+            `id` int NOT NULL AUTO_INCREMENT,
+            `time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+            `info` text,
+            PRIMARY KEY (`id`)
+            );
+        ");
+    }
     function check($username) {
         $statement = null;
         $this->sql_cmd = "SELECT * FROM account_list WHERE username=?";
